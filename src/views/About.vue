9b3f101e2e15d01ceb959d42e9f0f2fece538ff0
Pedro Monteiro <pedro.monteiro@valtech.com>
2020-02-11 14:16:52 -0300

    Finished VueJS crash course project - by Traversy Media

---
+++ src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is the TodoList app v1.0. It is part of the VueJS Crash Course on Youtube by Traversy Media.<br>
+    Made by <span>Pedro Monteiro</span></p>
+  </div>
+</template>
